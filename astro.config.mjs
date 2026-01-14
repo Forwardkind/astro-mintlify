67c924b51cd96349d3f6919f733c71c8d87a14db
jscarantino <j.scarantino@gmail.com>
2026-01-14 13:44:35 -0700

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,14 @@ export default defineConfig({
   site: 'https://docs.forwardkind.com',
   integrations: [
     starlight({
-      title: 'starmint',
+      title: 'Forwardkind',
       logo: {
-        light: './src/assets/dark-logo.svg',
+        light: './src/assets/fk-favicon.png',
         dark: './src/assets/light-logo.svg',
       },
       customCss: ['./src/styles/custom.css', './src/styles/tailwind.css'],
       social: {
-        github: 'https://github.com/alexwhitmore/astro-mintlify',
+        github: 'https://github.com/Forwardkind',
       },
       components: {
         Header: './src/components/Header.astro',
@@ -24,50 +24,100 @@ export default defineConfig({
           label: 'Getting Started',
           items: [
             {
-              label: 'Introduction',
-              slug: 'getting-started/introduction',
+              label: 'Overview & Welcome',
+              slug: 'getting-started/overview',
             },
             {
-              label: 'Quickstart',
-              slug: 'getting-started/quickstart',
-            },
-          ],
-        },
-        {
-          label: 'Essentials',
-          items: [
-            {
-              label: 'Creating Pages',
-              slug: 'essentials/creating-pages',
+              label: 'Who This Is For',
+              slug: 'getting-started/audience',
             },
             {
-              label: 'Theming',
-              slug: 'essentials/theming',
+              label: 'How to Use This Portal',
+              slug: 'getting-started/how',
             },
             {
-              label: 'Images',
-              slug: 'essentials/images',
+              label: 'Our Commitment',
+              slug: 'getting-started/commitment',
             },
             {
-              label: 'Deployments',
-              slug: 'essentials/deployments',
+              label: 'How to Get Involved',
+              slug: 'getting-started/get-involved',
             },
           ],
         },
         {
-          label: 'Components',
+          label: 'About Us',
           items: [
             {
-              label: 'Badge',
-              slug: 'components/badge',
+              label: 'Mission, Vision & Values',
+              slug: 'about-us/mission',
             },
             {
-              label: 'Asides',
-              slug: 'components/asides',
+              label: 'Co-op Structure & Model',
+              slug: 'about-us/model',
             },
             {
-              label: 'Tabs',
-              slug: 'components/tabs',
+              label: 'Legal Documents & Policies',
+              slug: 'about-us/legal-policy',
+            },
+          ],
+        },
+        {
+          label: 'Membership',
+          items: [
+            {
+              label: 'About Membership',
+              slug: 'membership/about-membership',
+            },
+            {
+              label: 'Member Benefits',
+              slug: 'membership/benefits',
+            },
+            {
+              label: 'Community Expectations',
+              slug: 'membership/community-expectations',
+            },
+          ],
+        },
+        {
+          label: 'Governance',
+          items: [
+            {
+              label: 'About Governance',
+              slug: 'governance/about-governance',
+            },
+            {
+              label: 'Decision Making',
+              slug: 'governance/decision-making',
+            },
+            {
+              label: 'Change & Amendments',
+              slug: 'governance/change-amendments',
+            },
+            {
+              label: 'Accountability',
+              slug: 'governance/accountability',
+            },
+            {
+              label: 'Financial Transparency',
+              slug: 'governance/financial-transparency',
+            },
+          ],
+        },
+        {
+          label: 'Resources',
+          items: [
+            {
+              label: 'Glossary',
+              slug: 'resources/glossary',
+            },
+            {
+              label: 'Learning & Education',
+              slug: 'resources/learning',
+            },
+            {
+              label: 'Tools We Use',
+              slug: 'resources/tools',
             },
           ],
         },
